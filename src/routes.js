d202901af61f243adc0967a736253767e0e06a5a
Juani <juanigeiler2001@gmail.com>
2025-07-12 18:55:32 +0000

    feat: new auth system, all get methods are public now

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { getArtists, getArtistById, addArtist, getArtistSongs, deleteArtist, updateArtist } = require('./controllers/artist');
 const { getSongs, addSong, deleteSong, updateSong } =  require('./controllers/song');
 const { register, login, verifyToken } = require('./controllers/auth');
-const { authenticateToken } = require('./middleware/auth');
+const { authenticateToken, optionalAuth } = require('./middleware/auth');
 
 const router = express.Router();
 
@@ -11,18 +11,18 @@ router.post('/auth/register', register);
 router.post('/auth/login', login);
 router.get('/auth/verify', authenticateToken, verifyToken);
 
-//artist
-router.get('/artists', authenticateToken, getArtists);
-router.get('/artists/:id', authenticateToken, getArtistById);
-router.put("/artists/:id", authenticateToken, updateArtist);
+//artist - GET routes are public, POST/PUT/DELETE require authentication
+router.get('/artists', getArtists);
+router.get('/artists/:id', getArtistById);
+router.get('/artists/:id/songs', getArtistSongs);
 router.post('/artists', authenticateToken, addArtist);
-router.get('/artists/:id/songs', authenticateToken, getArtistSongs);
+router.put("/artists/:id", authenticateToken, updateArtist);
 router.delete('/artists/:id', authenticateToken, deleteArtist);
 
-//songs
-router.get('/songs', authenticateToken, getSongs);
-router.post('/songs', authenticateToken,addSong);
-router.delete('/songs/:id', authenticateToken, deleteSong);
+//songs - GET routes are public, POST/PUT/DELETE require authentication
+router.get('/songs', getSongs);
+router.post('/songs', authenticateToken, addSong);
 router.put("/songs/:id", authenticateToken, updateSong);
+router.delete('/songs/:id', authenticateToken, deleteSong);
 
 module.exports = router;
